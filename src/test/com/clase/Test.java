ac466841e708dc5328a3fc79754df5a83bf2aa2e
s3bok <paznogard@gmail.com>
2015-11-18 14:35:18 -0600

    test2s3bok

--- src/test/com/clase/Test.java
+++ src/test/com/clase/Test.java
@@ -2,6 +2,15 @@ package test.com.clase;
 
 public class Test {
  String test;
+ int numero;
+
+public int getNumero() {
+	return numero;
+}
+
+public void setNumero(int numero) {
+	this.numero = numero;
+}
 
 public String getTest() {
 	return test;
